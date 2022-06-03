3f6bd9587bf72099b9ae63f167a600d76140f6fb
Thanh Dat <ngu.tki.sao99@gmail.com>
2022-06-03 22:27:08 +0700

    question 10

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const arr = ["one","two","three","four",'five']
+const sliceArr = arr.splice(3)
+arr.splice(2,0,...sliceArr)  
+console.log(arr)
+var nestArr = arr.reduce((a, c, i) => {
+    return i % 2 === 0 ? a.concat([arr.slice(i, i + 2)]) : a;
+  }, []);
 
-const groupByLength = (arr)=>{
-   
-}
+var result = nestArr.reduce((a,c,i)=>{
+    a[i+3] = c;
+    return a
+},{})
+
+console.log(result)
+
+
+  
